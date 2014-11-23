af5d50b85cdc94b89464c15d5c6943feef743918
rpwang <ruipengwang@gmail.com>
2014-11-23 12:12:57 +0100

    New Framework
    Durandal

---
+++ dev/app/viewmodels/form.js
@@ -0,0 +1,11 @@
+﻿define(function (require) {
+	var app = require('durandal/app'),
+	    ko = require('knockout');
+    //Note: This module exports an object.
+    return {
+    	name: ko.observable(),
+		sayHello:function(){
+			app.showMessage('Hello '+this.name() + '! Nice to meet you.','Greetings');
+		}
+    };
+});
