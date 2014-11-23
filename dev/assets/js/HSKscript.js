3d02b2994d3800ff1b415155898e124e0385b73f
rpwang <ruipengwang@gmail.com>
2014-11-23 12:14:07 +0100

    Merge branch 'master' of https://github.com/rpwang/learnHSK
    Conflicts:
    dev/assets/js/HSKscript.js
    dev/index.html

---
+++ dev/assets/js/HSKscript.js
@@ -0,0 +1,192 @@
+$( document ).ready(function() {
+	console.log( "ready!" );
+
+	//setting up the canvas model
+	var canvas = document.querySelector('canvas'), context = canvas.getContext('2d');
+	canvas.width = 500; //document.width is obsolete
+	canvas.height = 500; //document.height is obsolete
+	context.fillStyle = '#E4E4E4';
+		
+	//get ready for the setting config
+	function setmode(){
+	// event pressing play button
+		// check for all values have been filled in
+		// loading screen (ajax) when loaded go to play thats it
+		var url; //normal-hsk1-120-hsk2-0	
+	}
+	$(":button[name=mode]").click(function() {
+		alert($(this).val())
+		$( "#lol" ).remove();
+	});
+	
+	
+	// event handler menu1 button click
+	$( "#btn-mainsetting" ).click(function() {
+		//disapear/delete menu 1
+		//alert(mode)
+		var hsk1checkBoxBool = $(":checkbox[name=my-checkbox][id=hsk1]").prop('checked')
+		var hsk2checkBoxBool = $(":checkbox[name=my-checkbox][id=hsk2]").prop('checked')  
+		alert("check: "+hsk1checkBoxBool+hsk2checkBoxBool)
+		console.log($(":text[data-slider-id=hsk1DataSlider]").data('slider').getValue());
+	
+		
+		$( "div.jumbotron.mainsetting" ).remove();
+		//show menu 2
+	});
+	
+
+	
+	// event handler menu1 button click
+	$( "#btn-mainmenu" ).click(function() {
+		//disapear/delete menu 1
+		$( "div.jumbotron.mainmenu" ).remove();
+		//show menu 2
+	});
+	
+		
+	context.font = "300px arial";
+	context.fillStyle = "#B32C2C";
+	context.fillText("电脑",0, 350);
+	//context.fillRect(0, 0, 500, 500);
+
+	$('#hsk1Slider').slider({
+		formater: function(value) {
+			return 'Current value: ' + value;
+		}
+	});
+	
+	$("[name='my-checkbox']").bootstrapSwitch();
+
+	$('#hsk2Slider').slider({
+		formater: function(value) {
+			return 'Current value: ' + value;
+		}
+	});
+	$("[name='my-checkbox']").bootstrapSwitch();	
+
+
+
+	// loading the data json file
+	var ajaxtext = "";
+	var ajaxobject;
+	var dataURL = "../json/HSK1.json";
+	$.ajax({
+		type: "GET",
+		url: dataURL,
+		async:false,
+		dataType: 'json',
+		success: function ( response ) {
+			var object = JSON.parse( JSON.stringify(response));
+			ajaxtext = JSON.stringify(object);
+			ajaxobject = object;
+	
+		}
+	});
+
+	
+	
+
+	
+
+
+	function shuffle(o){ //v1.0
+		for(var j, x, i = o.length; i; j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+		return o;
+	};
+
+	function popJsonArray(array, property, value) {
+	   $.each(array, function(index, result) {
+	      if(result[property] == value) {
+	          //Remove from array
+	          array.splice(index, 1);
+	      }    
+	   });
+	}
+
+	function shuffleArray(array) {
+		for (var i = array.length - 1; i > 0; i--) {
+			var j = Math.floor(Math.random() * (i + 1));
+			var temp = array[i];
+			array[i] = array[j];
+			array[j] = temp;
+			}
+		return array;
+	}
+
+	function pickItemFromJson(json_object){
+		var keyArray = $.map(json_object, function (value, key) { return value; });
+		//console.log(keyArray)
+		var shuffle_keyArray = shuffleArray(keyArray)
+		return shuffle_keyArray;
+	}
+	var shuffle_keyArray = pickItemFromJson(ajaxobject);
+
+	function update(array){
+		//reseting canvas rectangle
+		context.fillStyle = '#E4E4E4';
+		context.fillRect(0, 0, 500, 500);
+		context.fill();
+		
+		//check length of array 
+		var lengthArray = array.length;
+		//Generating a random number in JavaScript raning between 0 and length of array
+		var randomnumber=Math.floor(Math.random()*(lengthArray));
+		
+		// pull random itemobject.hanzi from array
+		var hanzi = JSON.stringify(array[randomnumber].hanzi)
+		var han = hanzi.replace(/"/g,''); //"[apple,orange,pear]")
+		
+		if (han.length === 1){
+			context.font = "300px arial";
+			context.fillStyle = "#B32C2C";
+			context.fillText(han,95, 350);
+		}
+		else if (han.length === 2){
+			context.font = "240px arial";
+			context.fillStyle = "#B32C2C";
+			context.fillText(han,10, 350);
+		}
+		else if (han.length === 3){
+			context.font = "160px arial";
+			context.fillStyle = "#B32C2C";
+			context.fillText(han,10, 350);
+		}
+		else{
+				context.font = "120px arial";
+				context.fillStyle = "#B32C2C";
+				context.fillText(han,10, 350);
+		}
+		
+		var pinyin = JSON.stringify(array[randomnumber].pinyin)
+		var pin = pinyin.replace(/"/g,''); //"[apple,orange,pear]")
+		$( "p.pinyin" ).html("<strong>Pinyin:</strong> "+pin );
+		var english = JSON.stringify(array[randomnumber].yingyu)
+		var en = english.replace(/"/g,''); //"[apple,orange,pear]")
+		$( "p.english" ).html("<strong>English:</strong> "+en );
+		var helan = JSON.stringify(array[randomnumber].helan)
+		var nl = helan.replace(/"/g,''); //"[apple,orange,pear]")
+		$( "p.dutch" ).html("<strong>Dutch:</strong> "+nl );
+		array.splice(randomnumber, 1);
+	}
+
+	update(shuffle_keyArray);
+	$( "button#next" ).click(function() {
+		update(shuffle_keyArray)
+		//remove
+		//add	
+	});
+	$( "button#show" ).click(function() {
+//		  $( "p.hide" ).first().show( "fast", function showNext() {
+//		    $( this ).next( "p.hide" ).show( "fast", showNext );
+//		  });
+		
+		if ( $( "p.hide" ).is( ":hidden" ) ) {
+	    $( "p.hide" ).slideDown( "slow" );
+	  } //else {
+	    //$( "p.hide" ).hide();
+	  //}
+	});
+	$( "button#hide" ).click(function() {
+		$( "p.hide" ).slideUp( "slow");
+	});
+});
